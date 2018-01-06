ae68e2dedbfeade880f67e87e628aec1b220c2ca
Toni Mitrevski <toni@stativa.com.mk>
2018-01-06 17:07:18 +0100

    fir migration

--- database/migrations/2018_01_06_091357_create_courses_table.php
+++ database/migrations/2018_01_06_091357_create_courses_table.php
@@ -15,16 +15,16 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
-            $table->string('status');
-            $table->string('contact');
-            $table->text('details');
+            $table->string('title')->nullable();
+            $table->string('status')->nullable();
+            $table->string('contact')->nullable();
+            $table->text('details')->nullable();
             $table->json('location');
-            $table->string('instructors');
-            $table->date('start_date');
-            $table->date('end_date');
-            $table->boolean('is_remote');
-            $table->boolean('continuous');
+            $table->string('instructors')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->boolean('is_remote')->nullable();
+            $table->boolean('continuous')->nullable();
             $table->timestamps();
         });
     }
